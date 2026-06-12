01e53494d3cc11d91bc333900534714b1dc87a1d
Рубен Абраамян <r.abrahamyan@raketacn.ru>
2026-06-12 13:01:29 +0300

    init project

---
+++ app/Models/Employee.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Employee extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $guard = 'employee';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'position',
+        'department',
+        'phone',
+        'hire_date',
+        'is_active',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'hire_date' => 'date',
+        'is_active' => 'boolean',
+    ];
+}
